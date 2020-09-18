1988aa6683e1d2d3b2bbac536ca72cda3557976e
LowRiHao <haroic1997@gmail.com>
2020-09-18 17:43:09 +0800

    lambdas

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -20,6 +20,10 @@ public class Main {
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        printDeadlineUsingStreams(tasksData);
+        for (Task t:filterByString(tasksData,"11")){
+            System.out.println(t);
+        }
 
     }
 
@@ -45,5 +49,20 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlineUsingStreams(ArrayList<Task> tasksData){
+        tasksData.stream()
+                .filter((s)-> s instanceof Deadline)
+                .sorted((a,b) -> a.getDescription().toLowerCase().compareTo(b.getDescription().toLowerCase()))
+                .forEach(System.out::println);
+    }
+
+    public static ArrayList<Task> filterByString(ArrayList<Task> tasksData, String filterString){
+        ArrayList<Task> filteredTaskList = (ArrayList<Task>) tasksData.stream()
+                .filter((s)-> s.getDescription().contains(filterString))
+                .collect(Collectors.toList());
+
+        return filteredTaskList;
     }
 }
